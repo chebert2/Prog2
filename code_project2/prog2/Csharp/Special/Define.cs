3fd579c048eb89d3234943b2f7d1afcba6958e61
chebert2 <chebe46@lsu.edu>
2016-10-31 23:20:35 -0500

    basic idea in place

--- code_project2/prog2/Csharp/Special/Define.cs
+++ code_project2/prog2/Csharp/Special/Define.cs
@@ -1,3 +1,5 @@
+
+/**
 // Define -- Parse tree node strategy for printing the special form define
 
 using System;
@@ -14,5 +16,46 @@ namespace Tree
         }
     }
 }
+**/
+
+
+
+
+
+
+// this is a draft and incomplete form of define .
+
+
+// Define -- Parse tree node strategy for printing the special form define
+
+using System;
+
+namespace Tree
+{
+    public class Define : Special
+    {
+	public Define() { }
+
+        public override void print(Node t, int n, bool p)
+        {
+            Printer.printDefine(t, n, p);
+        }
+
+          // needs work.  It is incomplete because it does not evaluate the args to (define x y  expression
+		public override Node eval(Node cons_expression, Environment env1) {
+
+
+			// the key of a frame is represented by the alist.getCar().getCar().getName();
+
+			// the val of a frame is represented by the alist.getCar().getCdr
+
+			env1.define (cons_expression.getCdr().getCar(),  cons_expression.getCdr().getCdr().getCar());
+
+			return null;
+		}
+    }
+}
+
+
 
 
