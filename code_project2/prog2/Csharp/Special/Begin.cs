6f19af66f425b2bfa42a4c99d751e3119d0f30e2
mmatt26 <mmatt26@lsu.edu>
2016-11-07 14:50:16 -0600

    Update Begin.cs

--- code_project2/prog2/Csharp/Special/Begin.cs
+++ code_project2/prog2/Csharp/Special/Begin.cs
@@ -1,3 +1,4 @@
+
 // Begin -- Parse tree node strategy for printing the special form begin
 
 using System;
@@ -12,6 +13,35 @@ namespace Tree
         {
             Printer.printBegin(t, n, p);
         }
+	public Node eval(Node t, Environment env) {
+/**Node cdr = t.getCdr();
+
+            if (cdr == null)
+
+            {
+
+                Console.Error.WriteLine("Error: Begin is null (cdr)");
+
+                return Nil.getInstance();
+
+            }
+
+
+
+            Node car = evalCdr(cdr, null, e);
+
+            if (car == null)
+
+            {
+
+                Console.Error.WriteLine("Error: Begin is null (car)");
+
+            }
+*/
+            return car;
+		return null;
+
+	}
     }
 }
 
