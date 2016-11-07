29833b5e9ce668a4e18d0dab4e111262239edf3b
mmatt26 <mmatt26@lsu.edu>
2016-11-07 15:33:04 -0600

    Update If.cs

--- code_project2/prog2/Csharp/Special/If.cs
+++ code_project2/prog2/Csharp/Special/If.cs
@@ -12,6 +12,24 @@ namespace Tree
         {
             Printer.printIf(t, n, p);
         }
+	public Node eval(Node t, Environment env) {
+		Node cadr = t.getCdr().getCar();
+		Node csddr = t.getCdr().getCdr().getCar();
+		if (cond.eval(env).getBoolean()) {
+		     return exp.eval(env);
+		}
+		else if (!(t.getCdr().getCdr().getCdr()).isNull()) {
+
+			return exp.eval(env);
+
+		} 
+		else {
+
+			Console.Writeline("Error: Null");
+
+			return new Nil();
+
+		}
     }
 }
 
