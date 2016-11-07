8fb90d01ce581844d2d74fb91fd65e967fd92977
mmatt26 <mmatt26@lsu.edu>
2016-11-07 14:55:07 -0600

    Update Cond.cs

--- code_project2/prog2/Csharp/Special/Cond.cs
+++ code_project2/prog2/Csharp/Special/Cond.cs
@@ -1,3 +1,4 @@
+
 // Cond -- Parse tree node strategy for printing the special form cond
 
 using System;
@@ -12,6 +13,18 @@ namespace Tree
         { 
             Printer.printCond(t, n, p);
         }
+	public Node eval(Node t, Environment env) {
+           Node ex;
+           ex = t.getCdr();
+           while ((!(ex.getCar()).eval(env).getBoolean()) && (!ex.isNull())) {
+			exp = exp.getCdr();
+		}
+	   if (exp.isNull()) 
+		return new Nil();
+	   else
+	       return (exp.getCar().getCdr().getCar().eval(env));
+
+	}
     }
 }
 
