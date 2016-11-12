4a06549f1510125b32f9715a1b0f53aa3a02ff0b
chebert2 <chebe46@lsu.edu>
2016-11-12 14:36:42 -0600

    updated   print   for Display BuiltIn case.

--- code_project2/prog2/Csharp/Tree/StringLit.cs
+++ code_project2/prog2/Csharp/Tree/StringLit.cs
@@ -15,22 +15,30 @@ namespace Tree
 
         public override void print(int n)
         {
-            Printer.printStringLit(n, stringVal);
+			// special StringLit print case     where String lit print will not print double quotes
+			if (Tree.BuiltIn.builtIn_display__do_not_print_double_quotes) {
+				// There got to be a more efficient way to print n spaces.
+				for (int i = 0; i < n; i++)
+					Console.Write (" ");
+				Console.Write (this.stringVal);
+			}
+
+			Printer.printStringLit(n, stringVal);
+
         }
 
         public override bool isString()
         {
             return true;
         }
-        
-	public String getString(){
-	     return stringVal;
-	}
-	
-        public override Node eval(Environment env1){
+
+		public String getString(){
+			return stringVal;
+		}
+
+		public override Node eval(Environment env1){
 			return this;
 		}
-        
     }
 }
 
